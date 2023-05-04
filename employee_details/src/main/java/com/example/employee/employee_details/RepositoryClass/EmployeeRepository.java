3ab7752515b64c8c549708d75ef80d1707e97e84
Praveen Allam <praveenallam93@gmail.com>
2023-05-04 19:00:18 +0530

    Delete employee_details directory

--- employee_details/src/main/java/com/example/employee/employee_details/RepositoryClass/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.employee.employee_details.RepositoryClass;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.employee.employee_details.EntityClass.EmployeeDetails;
-
-public interface EmployeeRepository extends JpaRepository<EmployeeDetails, String>{
-    
-}
